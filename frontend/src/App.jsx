cf0f264138bd9e5f789b501dbc6e5553e4369eee
Relebogile <chiloane.rk@gmail.com>
2024-04-08 13:11:31 +0200

    Axios usage in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
   const url = "https://gec-api.azurewebsites.net/users";
-  fetch(url).then(res => res.json()).then( data => console.log(data));
+
+  axios.get(`${url}/users`).then((response) => {
+    console.log(response);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Check console to see if it works under /users
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
