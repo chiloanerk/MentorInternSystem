4f11d81467a1d48a8dc593bfba5ea708455b8f66
Relebogile <chiloane.rk@gmail.com>
2024-04-08 13:53:36 +0200

    Changed data presentation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,7 +4,14 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
-    user: 'user 1'
+    user1: 'Christabel',
+    user2: 'Lithini',
+    user3: 'Mish-Al',
+    user4: 'Michael',
+    user5: 'Uthmaan',
+    user6: 'Vika',
+    user7: 'Rele',
+    user8: 'Anele',
   });
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
   const baseUrl = 'https://gec-api.azurewebsites.net';
+  const [users, setUsers] = useState([]);
 
-  axios.get(  baseUrl + '/users').then((response) => {
-    console.log(response.data);
-  });
+  useEffect(() => {
+    axios.get(baseUrl + '/users')
+        .then((response) => {
+          setUsers(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching users:', error);
+        });
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Check console to see if it works under /users
-        </p>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            If it works, the users will appear below.
+          </p>
+          <ul>
+            {users.map((user, index) => (
+                <li key={index}>{user}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
   );
 }
 
